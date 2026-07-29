dfb597cf8efb8982dce557def750a0d3c84f9529
rindhyaranisenthilkumar-svg <rindhyaranisenthilkumar@gmail.com>
2026-07-29 11:28:47 +0530

    Refactor Routes and Route syntax in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
 import MyCounter from "./Counter";
-import {Add} from './Math'
-import {Routes,Route}from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-  <Routes>
-    <Route path='/counter' element={<MyCounter/>}/>
-   
-    <Route path="*"element={<h1>Incorrect Path</h1>}/>
-  </Routes>
+      <Routes>
+        <Route path="/counter" element={<MyCounter />} />
+        <Route path="*" element={<h1>Incorrect Path</h1>} />
+      </Routes>
     </>
   );
 }
